f16fe1e79b1d0ab16b9c1f742bec9d0089a4f609
Harish Manyam <harish.manyam@cognizant.com>
2020-04-26 21:51:00 +0530

    initial commit

---
+++ product-service/src/test/java/com/cognizant/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
