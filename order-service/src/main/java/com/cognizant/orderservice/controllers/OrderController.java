1fcfc95684a94426d13e2f2b5af246cf02967672
Harish Manyam <harish.manyam@cognizant.com>
2020-04-30 14:40:03 +0530

    added orders rest controller end points

--- order-service/src/main/java/com/cognizant/orderservice/controllers/OrderController.java
+++ order-service/src/main/java/com/cognizant/orderservice/controllers/OrderController.java
@@ -1,7 +1,36 @@
 package com.cognizant.orderservice.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OrderController {
+
+    @GetMapping("orders/:id")
+    public ResponseEntity<Object> getOneOrder() //add orders object
+    {
+        return  null;
+    }
+    @GetMapping("orders")
+    public ResponseEntity<Object> getAllOrders()//add ordersObject
+    {
+        return null;
+    }
+    @PostMapping("orders")
+    public ResponseEntity<Boolean> addOneOrder() //add orders Object in the method parameter
+    {
+        return  null;
+    }
+
+    @PutMapping("orders/action/accept/:id")
+    public ResponseEntity<Boolean> acceptOrder(@PathVariable Integer id)
+    {
+        return null;
+    }
+
+    @PutMapping("orders/action/reject")
+    public ResponseEntity<Boolean> rejectOrder(@PathVariable Integer id)
+    {
+        return null
+    }
 }
