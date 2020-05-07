424cff1239ebdee84d005d3e7eeee885ad20aa0f
unknown <mandavaravindra1998@gmail.com>
2020-05-07 13:55:58 +0530

    order-service serviceImpl methods added

--- order-service/src/main/java/com/cognizant/orderservice/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/cognizant/orderservice/service/OrderServiceImpl.java
@@ -1,5 +1,82 @@
 package com.cognizant.orderservice.service;
 
-public class OrderServiceImpl {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cognizant.orderservice.entity.OrderEntity;
+import com.cognizant.orderservice.model.OrderModel;
+import com.cognizant.orderservice.repository.OrderRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+	@Autowired
+	OrderRepository orderRepo;
+	
+	
+	@Override
+	public boolean addOrder(OrderModel orderModel) {
+		OrderEntity order =  new OrderEntity();
+		order.setUserId(orderModel.getUserId());
+		order.setOrderAmount(orderModel.getOrderAmount());
+		order.setOrderDate(orderModel.getOrderDate());
+		order.setOrderStatus(orderModel.getOrderStatus());
+		order.setOrderType(orderModel.getOrderType());
+		
+		order = orderRepo.save(order);
+		orderModel.setOrderId(order.getOrderId());
+		
+		return true;
+	}
+
+	@Override
+	public List<OrderModel> getUsersOrders(int userId) {
+		
+		List<OrderModel> allOrders = new ArrayList();
+		Iterable<OrderEntity> allOrdersEntity = orderRepo.findByUserId(userId);
+		Iterator<OrderEntity> itr = allOrdersEntity.iterator();
+		while(itr.hasNext()) {
+			OrderEntity temp = itr.next();
+			OrderModel orderModel = new OrderModel(
+													temp.getOrderId(),
+													temp.getOrderAmount(),
+													temp.getOrderStatus(),
+													temp.getOrderDate(),
+													temp.getUserId(),
+													temp.getOrderType()
+												   );
+			
+			allOrders.add(orderModel);
+		}
+		
+		
+		return allOrders;
+	}
+
+	@Override
+	public List<OrderModel> allOrders() {
+		
+		List<OrderModel> allOrders = new ArrayList();
+		Iterable<OrderEntity> allOrdersEntity = orderRepo.findAll();
+		Iterator<OrderEntity> itr = allOrdersEntity.iterator();
+		while(itr.hasNext()) {
+			OrderEntity temp = itr.next();
+			OrderModel orderModel = new OrderModel(
+													temp.getOrderId(),
+													temp.getOrderAmount(),
+													temp.getOrderStatus(),
+													temp.getOrderDate(),
+													temp.getUserId(),
+													temp.getOrderType()
+												   );
+			
+			allOrders.add(orderModel);
+		}
+		
+		
+		return allOrders;
+	}
 
 }
