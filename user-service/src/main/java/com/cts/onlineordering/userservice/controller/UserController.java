85097ee345990297297c1385b65d3b21b99e057b
pravallika <pravallikasadhu97@gmail.com>
2020-05-09 11:20:42 +0530

    change

--- user-service/src/main/java/com/cts/onlineordering/userservice/controller/UserController.java
+++ user-service/src/main/java/com/cts/onlineordering/userservice/controller/UserController.java
@@ -10,16 +10,16 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD:user-service/src/main/java/com/cts/onlineordering/userservice/controller/UserController.java
+
 import org.springframework.web.bind.annotation.*;
-=======
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> 151e35d27dc1dea902cc659827acfb27cd3bb314:user-service/src/main/java/com/cts/onlineordering/userservice/controllers/UserController.java
+
 
 
 @RestController
@@ -55,7 +55,7 @@ public class UserController implements IUserController {
         // user log-in user here
         return new ResponseEntity<>(true,HttpStatus.OK);
     }
-<<<<<<< HEAD:user-service/src/main/java/com/cts/onlineordering/userservice/controller/UserController.java
+
     @GetMapping("/getUserId")
     public  ResponseEntity<Integer> getUserId(@RequestHeader HttpHeaders headers)
     {
@@ -64,7 +64,7 @@ public class UserController implements IUserController {
         Integer id = this.userService.getUserId(user);
         return new ResponseEntity<>(id,HttpStatus.OK);
     }
-=======
+
     
     @ExceptionHandler  // ~catch
 	public ResponseEntity<UserErrorResponse> productOperationErrorHAndler(Exception ex) {
@@ -78,6 +78,6 @@ public class UserController implements IUserController {
 		
 		return response;
 	}
->>>>>>> 151e35d27dc1dea902cc659827acfb27cd3bb314:user-service/src/main/java/com/cts/onlineordering/userservice/controllers/UserController.java
+
 
 }
