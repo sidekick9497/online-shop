e920fd8a95aed45d7ef063e797585b132ace9599
unknown <mandavaravindra1998@gmail.com>
2020-05-14 19:44:49 +0530

    user controller altered

--- user-service/src/main/java/com/cts/onlineordering/userservice/controller/UserController.java
+++ user-service/src/main/java/com/cts/onlineordering/userservice/controller/UserController.java
@@ -10,10 +10,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
 
-=======
->>>>>>> 3fd2a3a6d16d11babcb429baba4a00cc13b44593
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
@@ -22,11 +19,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-<<<<<<< HEAD
-=======
-
->>>>>>> 3fd2a3a6d16d11babcb429baba4a00cc13b44593
-
 
 @RestController
 
@@ -61,11 +53,9 @@ public class UserController implements IUserController {
         // user log-in user here
         return new ResponseEntity<>(true,HttpStatus.OK);
     }
-<<<<<<< HEAD
- @GetMapping("/getUserId")
-=======
+
+    	
     @GetMapping("/getUserId")
->>>>>>> 3fd2a3a6d16d11babcb429baba4a00cc13b44593
     public  ResponseEntity<Integer> getUserId(@RequestHeader HttpHeaders headers)
     {
         String user= headers.get("user-name").get(0);
@@ -73,11 +63,7 @@ public class UserController implements IUserController {
         Integer id = this.userService.getUserId(user);
         return new ResponseEntity<>(id,HttpStatus.OK);
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 3fd2a3a6d16d11babcb429baba4a00cc13b44593
-    
+   
     @ExceptionHandler  // ~catch
 	public ResponseEntity<UserErrorResponse> productOperationErrorHAndler(Exception ex) {
 		// create error object
